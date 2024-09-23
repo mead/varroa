ee531aae866996e6d7e1fd5d234d0de3036eddae
Sam Morrison <sorrison@gmail.com>
2024-09-23 12:45:01 +1000

    Add managment command to backfill ip usage
    Change-Id: I6ada8a9d8fbe7254da8a8a9eeec222efeca3edee

--- varroa/cmd/manage.py
+++ varroa/cmd/manage.py
@@ -12,12 +12,73 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import datetime
+
 import click
 from flask.cli import FlaskGroup
+from oslo_log import log as logging
 
 from varroa import app
+from varroa.common import clients
+from varroa.common import keystone
+from varroa.common import utils
+from varroa.extensions import db
+from varroa import models
+
+
+LOG = logging.getLogger(__name__)
 
 
 @click.group(cls=FlaskGroup, create_app=app.create_app)
 def cli():
     """Management script for the Warre application."""
+
+
+@cli.command('backfill-ports')
+def backfill_ports():
+    k_session = keystone.KeystoneSession().get_session()
+    openstack = clients.get_openstack(k_session)
+
+    all_ports = openstack.list_ports()
+
+    for port in all_ports:
+        ip_usage = (
+            db.session.query(models.IPUsage)
+            .filter_by(port_id=port.id)
+            .one_or_none()
+        )
+        if ip_usage is None:
+            if port.device_owner.startswith("compute:"):
+                resource_type = "instance"
+            else:
+                LOG.warning(
+                    "Port %s device owner %s not supported",
+                    port.id,
+                    port.device_owner,
+                )
+                continue
+
+            try:
+                ipaddress = port.fixed_ips[0].get("ip_address")
+            except Exception:
+                LOG.error("Port %s has no ipaddress", port.id)
+                continue
+
+            if utils.is_private_ip(ipaddress):
+                LOG.debug("Skipping private IP %s", ipaddress)
+                continue
+
+            port_created = datetime.datetime.strptime(
+                port.created_at, "%Y-%m-%dT%H:%M:%SZ"
+            )
+            ip_usage = models.IPUsage(
+                ip=ipaddress,
+                project_id=port.project_id,
+                port_id=port.id,
+                resource_id=port.device_id,
+                resource_type=resource_type,
+                start=port_created,
+            )
+
+            db.session.add(ip_usage)
+            db.session.commit()
